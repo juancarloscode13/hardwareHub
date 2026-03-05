ffcbf1476be0bb0a6ec9d86ac7ef8d9837629ea8
juancarloscode13 <jsanmiguelcarmona@gmail.com>
2026-03-05 18:46:45 +0100

    primer commit

---
+++ src/main/java/com/juanCarlos/hardwareHub/HardwareHubApplication.java
@@ -0,0 +1,13 @@
+package com.juanCarlos.hardwareHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardwareHubApplication.class, args);
+	}
+
+}
