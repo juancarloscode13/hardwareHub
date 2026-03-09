0a183b893fb7d5c3f37d1c227530ff54da90e13f
juancarloscode13 <jsanmiguelcarmona@gmail.com>
2026-03-09 10:40:05 +0100

    añadidos metodos necesarios en algunos repositorios (revisar)

--- src/main/java/com/juanCarlos/hardwareHub/repository/RamRepository.java
+++ src/main/java/com/juanCarlos/hardwareHub/repository/RamRepository.java
@@ -1,7 +1,11 @@
 package com.juanCarlos.hardwareHub.repository;
 
 import com.juanCarlos.hardwareHub.entity.RamEntity;
+import com.juanCarlos.hardwareHub.entity.enums.RamTipo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RamRepository extends JpaRepository<RamEntity, Long> {
+    List<RamEntity> getByTipo(RamTipo tipo);
 }
