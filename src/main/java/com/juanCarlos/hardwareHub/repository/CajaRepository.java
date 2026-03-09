8e73fba1e11ee382265ed5695e4f2e10def81ee7
juancarloscode13 <jsanmiguelcarmona@gmail.com>
2026-03-09 10:40:05 +0100

    añadidos metodos necesarios en algunos repositorios (revisar)

--- src/main/java/com/juanCarlos/hardwareHub/repository/CajaRepository.java
+++ src/main/java/com/juanCarlos/hardwareHub/repository/CajaRepository.java
@@ -1,7 +1,16 @@
 package com.juanCarlos.hardwareHub.repository;
 
 import com.juanCarlos.hardwareHub.entity.CajaEntity;
+import com.juanCarlos.hardwareHub.entity.enums.CajaFormato;
+import com.juanCarlos.hardwareHub.entity.enums.PsuFactorForma;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CajaRepository extends JpaRepository<CajaEntity, Long> {
+    List<CajaEntity> getByPlacasBaseCompatibles(CajaFormato placasBaseCompatibles);
+
+    List<CajaEntity> getByPsuCompatible(PsuFactorForma psuCompatible);
+
+    List<CajaEntity> getByAlturaMaxEnfriadorCpuGreaterThanEqual(Integer alturaMaxEnfriadorCpu);
 }
--- src/main/java/com/juanCarlos/hardwareHub/repository/PsuRepository.java
+++ src/main/java/com/juanCarlos/hardwareHub/repository/PsuRepository.java
@@ -1,7 +1,13 @@
 package com.juanCarlos.hardwareHub.repository;
 
 import com.juanCarlos.hardwareHub.entity.PsuEntity;
+import com.juanCarlos.hardwareHub.entity.enums.PsuFactorForma;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PsuRepository extends JpaRepository<PsuEntity, Long> {
+    List<PsuEntity> getByFactorForma(PsuFactorForma factorForma);
+
+    List<PsuEntity> getByPotenciaGreaterThanEqual(Integer potencia);
 }
