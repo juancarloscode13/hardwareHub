1439bc15ac5bbf5415e5a5d7975d636e24862111
juancarloscode13 <jsanmiguelcarmona@gmail.com>
2026-03-10 13:07:09 +0100

    creadas clases necesarias para dsl, revisar y finalizar implementacion

---
+++ src/main/java/com/juanCarlos/hardwareHub/dsl/parser/QueryDslParser.java
@@ -0,0 +1,40 @@
+package com.juanCarlos.hardwareHub.dsl.parser;
+
+import com.juanCarlos.hardwareHub.dsl.model.FilterCriteria;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class QueryDslParser {
+
+    private static final Pattern PATTERN =
+            Pattern.compile("(\\w+)(==|!=|>=|<=|>|<|~)(.+)");
+
+    public List<FilterCriteria>parse(String query){
+        List<FilterCriteria>filters = new ArrayList<>();
+
+        if (query == null || query.isEmpty())
+            return filters;
+
+        String[]expressions = query.split(";");
+
+        for (String exp : expressions){
+
+            Matcher matcher = PATTERN.matcher(exp);
+
+            if (matcher.find()){
+
+                filters.add(
+                        new FilterCriteria(
+                                matcher.group(1),
+                                matcher.group(2),
+                                matcher.group(3)
+                        )
+                );
+            }
+        }
+        return filters;
+    }
+}
