41913df27320fc75d20e76a628c9faa78f9c7aa5
juancarloscode13 <jsanmiguelcarmona@gmail.com>
2026-03-25 13:29:50 +0100

    arreglado bug del doble archivo de logging

--- src/main/java/com/juanCarlos/hardwareHub/HardwareHubApplication.java
+++ src/main/java/com/juanCarlos/hardwareHub/HardwareHubApplication.java
@@ -2,7 +2,26 @@ package com.juanCarlos.hardwareHub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
+@EnableScheduling
 public class HardwareHubApplication {
-	public static void main(String[] args) {SpringApplication.run(HardwareHubApplication.class, args);}}
+
+	public static void main(String[] args) {
+		/*
+		 * Fija el nombre del fichero de log UNA SOLA VEZ por arranque.
+		 * Esto se hace asi para evitar problemas de archivos duplicados de log.
+		 */
+		if (System.getProperty("app.log.file") == null) {
+			String timestamp = LocalDateTime.now()
+					.format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss-SSS"));
+			System.setProperty("app.log.file",
+					System.getProperty("user.dir") + "/log/log_" + timestamp + ".txt");
+		}
+		SpringApplication.run(HardwareHubApplication.class, args);
+	}
+}
